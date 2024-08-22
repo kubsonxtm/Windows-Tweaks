4ef54cab8931ddac6871e62c8abb71b14a55897b
kubsonxtm <138996574+kubsonxtm@users.noreply.github.com>
2024-08-22 15:26:44 +0200

    Update run this first.bat

--- 4 Programs to tweak or increase privacy/3 Registry/run this first.bat
+++ 4 Programs to tweak or increase privacy/3 Registry/run this first.bat
@@ -1,7 +1,17 @@
 @echo off
-REM Disable scheduled tasks
+REM DIASBLE FIREFOX DEAFULT BROWSER AGENT
 schtasks.exe /change /disable /tn "\Mozilla\Firefox Default Browser Agent 308046B0AF4A39CB"
 schtasks.exe /change /disable /tn "\Mozilla\Firefox Default Browser Agent D2CEEC440E2074BD"
+
+REM DISABLE TELEMETRY TASKS FOR NVIDIA
+schtasks.exe /change /tn NvTmRepOnLogon_{B2FE1952-0186-46C3-BAEC-A80AA35AC5B8} /disable
+schtasks.exe /change /tn NvTmRep_{B2FE1952-0186-46C3-BAEC-A80AA35AC5B8} /disable
+schtasks.exe /change /tn NvTmMon_{B2FE1952-0186-46C3-BAEC-A80AA35AC5B8} /disable
+
+REM NVIDIA TELEMETRY SERVICE DISABLE
+net.exe stop NvTelemetryContainer
+sc.exe config NvTelemetryContainer start= disabled
+sc.exe stop NvTelemetryContainer
 
 REM Get the directory of this script
 set "ScriptDir=%~dp0"
