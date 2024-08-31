7d9215b4bee7e4c19a871a52b771100855488d0a
kubsonxtm <138996574+kubsonxtm@users.noreply.github.com>
2024-08-31 18:55:34 +0200

    Delete 3 Automatic installation directory

--- 3 Automatic installation/Spotify (without ads)/Spotify.ps1
+++
@@ -1,1 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression "& { $(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1') } -UninstallSpotifyStoreEdition -UpdateSpotify"
