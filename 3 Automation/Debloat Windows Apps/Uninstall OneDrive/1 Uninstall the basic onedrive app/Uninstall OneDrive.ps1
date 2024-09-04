6fdce966f8160dfa0b5d697069ef5e6daac5d77c
kubsonxtm <138996574+kubsonxtm@users.noreply.github.com>
2024-09-04 21:46:38 +0200

    Add files via upload

---
+++ 3 Automation/Debloat Windows Apps/Uninstall OneDrive/1 Uninstall the basic onedrive app/Uninstall OneDrive.ps1
@@ -0,0 +1,31 @@
+Write-Output "Kill OneDrive process"
+taskkill.exe /F /IM "OneDrive.exe"
+taskkill.exe /F /IM "explorer.exe"
+
+Write-Output "Removing OneDrive"
+$uninstallCompleted = $false
+
+if (Test-Path "$env:systemroot\System32\OneDriveSetup.exe") {
+    Write-Output "Uninstalling OneDrive from System32"
+    & "$env:systemroot\System32\OneDriveSetup.exe" /uninstall
+    $uninstallCompleted = $true
+}
+
+if (Test-Path "$env:systemroot\SysWOW64\OneDriveSetup.exe") {
+    Write-Output "Uninstalling OneDrive from SysWOW64"
+    & "$env:systemroot\SysWOW64\OneDriveSetup.exe" /uninstall
+    $uninstallCompleted = $true
+}
+
+if ($uninstallCompleted) {
+    Write-Output "Waiting for OneDrive uninstall to complete"
+    Start-Sleep -Seconds 10
+
+    Write-Output "Restarting explorer"
+    Start-Process "explorer.exe"
+    Write-Output "OneDrive has been successfully uninstalled."
+} else {
+    Write-Output "OneDrive was not found or was not uninstalled."
+}
+
+Read-Host -Prompt "Press Enter to exit"
