2f1cf110db24735b485d3a38e096b933a6dc05e1
kubsonxtm <138996574+kubsonxtm@users.noreply.github.com>
2024-11-15 13:33:12 +0100

    Update Cache Cleaner.bat

--- 6 Cleaning/Cache Cleaner/Cache Cleaner.bat
+++ 6 Cleaning/Cache Cleaner/Cache Cleaner.bat
@@ -17,7 +17,6 @@ echo                                                        CLEANING
 echo.
 echo.
 
-:: Cleaning %temp%
 for /r "%temp%" %%f in (*) do (
     echo Removing %%f
     del /f /q "%%f"
@@ -27,7 +26,6 @@ for /d %%d in ("%temp%\*") do (
 )
 mkdir "%temp%"
 
-:: Cleaning Windows Temp
 for /r "C:\Windows\Temp" %%f in (*) do (
     echo Removing %%f
     del /f /q "%%f"
@@ -37,58 +35,47 @@ for /d %%d in ("C:\Windows\Temp\*") do (
 )
 mkdir "C:\Windows\Temp"
 
-:: Cleaning Prefetch
 takeown /f "%SystemRoot%\Prefetch" /r /d y > NUL 2>&1
 
 icacls "%SystemRoot%\Prefetch" /grant "%username%:(OI)(CI)F" /T /C > NUL 2>&1
 
-:: Deleting files from Prefetch
 for /f "delims=" %%f in ('dir /b /a-d "%SystemRoot%\Prefetch\*"') do (
     echo Removing %%f
     del /f /q "%SystemRoot%\Prefetch\%%f"
 )
 
-:: Deleting subdirectories from Prefetch
 for /d %%d in ("%SystemRoot%\Prefetch\*") do (
     echo Removing directory %%d
     rd /s /q "%%d"
 )
 
-:: Cleaning SoftwareDistribution\Download
 takeown /f "%SystemRoot%\SoftwareDistribution\Download" /r /d y > NUL 2>&1
 
 icacls "%SystemRoot%\SoftwareDistribution\Download" /grant "%username%:(OI)(CI)F" /T /C > NUL 2>&1
 
-:: Deleting files from SoftwareDistribution\Download
 for /r "%SystemRoot%\SoftwareDistribution\Download" %%f in (*) do (
     echo Removing %%f
     del /f /q "%%f"
 )
 
-:: Deleting subdirectories from SoftwareDistribution\Download
 for /d %%d in ("%SystemRoot%\SoftwareDistribution\Download\*") do (
     echo Removing directory %%d
     rd /s /q "%%d"
 )
 
-:: Cleaning LiveKernelReports
 takeown /f "%SystemRoot%\LiveKernelReports" /r /d y > NUL 2>&1
 
 icacls "%SystemRoot%\LiveKernelReports" /grant "%username%:(OI)(CI)F" /T /C > NUL 2>&1
 
-:: Deleting files from LiveKernelReports
 for /r "%SystemRoot%\LiveKernelReports" %%f in (*) do (
     echo Removing %%f
     del /f /q "%%f"
 )
 
-:: Start Disk Cleanup with options
 cleanmgr /sageset:1
 
-:: Run Disk Cleanup with selected options
 start /wait cleanmgr /sagerun:1
 
-:: After Disk Cleanup finishes
 echo.
 echo Cleanup completed
 echo.
